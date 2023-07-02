0daba9d12349f01f68378c6c5042be0ff1c019c6
Samet Özkan <samet-ozkan@outlook.com>
2023-07-02 21:03:53 +0300

    Project created

---
+++ app/src/main/java/com/sametozkan/notepadapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.sametozkan.notepadapp
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        println("Hello!")
+    }
+
+}
